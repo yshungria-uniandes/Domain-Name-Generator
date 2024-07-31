15a614784117e5614e3932e0a76b243903ffac9b
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-07-30 22:06:44 -0500

    Project Finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log(i, pronoun[i]); // 0 the, 1 our
+    for (let j = 0; j < adj.length; j++) {
+      console.log(j, adj[j]); // 0 great, 1 big
+      for (let k = 0; k < noun.length; k++) {
+        console.log(k, noun[k]); // 0 jogger, 1 racoon
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+      }
+    }
+  }
 };
