091e7c2895897f25c610af8f1550f5d9584313bc
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-07-30 22:27:17 -0500

    Project Finished Domain Name Generator with Boostrap and Console.log

--- src/app.js
+++ src/app.js
@@ -11,14 +11,39 @@ window.onload = function() {
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
 
+  // Get the domain list container
+  let domainList = document.getElementById("domain-list");
+
   for (let i = 0; i < pronoun.length; i++) {
-    console.log(i, pronoun[i]); // 0 the, 1 our
+    // console.log(i, pronoun[i]); // 0 the, 1 our
     for (let j = 0; j < adj.length; j++) {
-      console.log(j, adj[j]); // 0 great, 1 big
+      // console.log(j, adj[j]); // 0 great, 1 big
       for (let k = 0; k < noun.length; k++) {
-        console.log(k, noun[k]); // 0 jogger, 1 racoon
+        // console.log(k, noun[k]); // 0 jogger, 1 racoon
         console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+
+        let domain = `${pronoun[i]}${adj[j]}${noun[k]}.com`;
+
+        // Create a new list item for each domain
+        let domainItem = document.createElement("a");
+        domainItem.className =
+          "list-group-item list-group-item-action list-group-item-info";
+        domainItem.textContent = domain;
+
+        // Add the domain to the container
+        domainList.appendChild(domainItem);
       }
     }
   }
 };
+
+// Should generate something like:
+
+// thegreatjogger.com
+// thegreatracoon.com
+// ourgreatjogger.com
+// ourgreatracoon.com
+// thebigjogger.com
+// thebigracoon.com
+// ourbigjogger.com
+// ourbigracoon.com
